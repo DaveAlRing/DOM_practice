565eae604adbe5d61ed3d3487dbd55bdf9c6d8b2
Dave Ring <davealring@gmail.com>
2024-07-12 08:50:05 -0400

    fixed typo in script

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const container = document.quesrySelector("#container");
+const container = document.querySelector("#container");
 
 const content = document.createElement("div");
 content.classList.add("content");
